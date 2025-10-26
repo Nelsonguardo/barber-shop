175f41a6071fc57b2bf1d51c1928e452c5f4dfc8
Nelsonguardo <guardonei4@gmail.com>
2025-10-25 19:06:16 -0500

    Activate hamburger menu on mobile and reduce image height on Home page

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const year = new Date().getFullYear();
 export const Footer = () => {
     return (
         <> 
-            <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-[#221510] text-white border-t border-[#D44211] shadow-sm md:flex md:items-center md:justify-between md:p-6">
+            <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-[#221510] text-white shadow-sm md:flex md:items-center md:justify-between md:p-6">
                 <p className="text-sm text-gray-500  dark:text-gray-400">© {year} Mi Barbería. Todos los derechos reservados.</p>
             </footer>
         </>
